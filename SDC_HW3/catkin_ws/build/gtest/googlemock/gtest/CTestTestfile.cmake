04a236627dfde5a0908326799e2513d843122e30
yelloooowww <cococing0324@gmail.com>
2020-10-26 20:42:59 +0800

    modify report

--- SDC_HW3/catkin_ws/build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/yellow/self-driving-cars-course/SDC_HW3/catkin_ws/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
